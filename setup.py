9f984d8ec31101497e33d2411ad58b3ca2d10c9e
Aaron Christianson <ninjaaron@gmail.com>
2018-10-08 10:47:28 +0200

    ready for pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+package = 'lazycli'
+version = '0.1.0'
+with open('README.rst') as fh:
+    long_description = fh.read()
+
+setup(name=package,
+      version=version,
+      description="generate command-line interfaces from function signatures",
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      url='https://github.com/ninjaaron/lazycli',
+      packages=['lazycli'],
+      python_requires='>=3.5')
