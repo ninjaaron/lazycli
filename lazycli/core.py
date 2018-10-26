8ee3ee2b353628d151e65816501349d6d47399bb
Aaron Christianson <ninjaaron@gmail.com>
2018-10-26 19:48:37 +0200

    unfinished refactor

--- lazycli/core.py
+++ lazycli/core.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import argparse
-import functools
+import collections
 import json
 import inspect
 import io
@@ -39,6 +39,9 @@ def sort_params(params):
     return positionals, flags, options
 
 
+ArgType = collections.namedtuple('ArgType', 'iterable, constructor')
+
+
 def isiterable(param, T):
     if param.kind == param.VAR_POSITIONAL:
         return True
@@ -55,6 +58,58 @@ def ismapping(param, T):
         return issubclass(T, t.Mapping)
     except (TypeError, AttributeError):
         return isinstance(param.default, t.Mapping)
+
+
+def real_type(T):
+    if T is object or issubclass(T, t.Mapping):
+        return ArgType(False, json.loads)
+
+    if issubclass(T, (io.IOBase, str)):
+        return ArgType(False, T)
+
+    if issubclass(T, t.Sequence):
+        return ArgType(True, None)
+
+    return ArgType(False, T)
+
+
+def typing_type(T):
+    iterable, _ = real_type(T)
+    if iterable:
+        try:
+            subscript = T.__args__[0]
+            return ArgType(True, T.__args__[0])
+        except IndexError:
+            return ArgType(True, None)
+
+    return ArgType(False, None)
+
+
+def annotation_type(annotation):
+    if isinstance(annotation, type):
+        return real_type(annotation)
+    return typing_type(annotation)
+
+
+def varargs_type(param):
+    if param.annotation is param.empty:
+        return ArgType(True, None)
+
+    _, constructor = annotation_type(param.annotation)
+    return ArgType(True, constructor)
+
+
+def infer_type(param, positional=False):
+    if param.kind is param.VAR_POSITIONAL:
+        return varargs_type(param)
+
+    if param.annotation is not param.empty:
+        return annotation_type(param.annotation)
+
+    if positional:
+        return None
+
+    return default_type(param.default)
 
 
 def add_arg(parser, name, param, kwargs, shortflag=None):
@@ -135,14 +190,12 @@ def mkoptions(params, parser):
         )
 
 
-def script(func=None, **kwargs):
-    if func is None:
-        return functools.partial(script, **kwargs)
-
+def script(func):
     def run(*args, **kwargs):
         """run as cli script. *args and **kwargs are pased to
         ArgumentParser.parse_args
         """
+        # build the parser from signature
         sig = inspect.signature(func)
         positionals, flags, options = sort_params(sig.parameters.values())
         parser = argparse.ArgumentParser(description=func.__doc__, **kwargs)
@@ -150,8 +203,10 @@ def script(func=None, **kwargs):
         mkflags(flags, parser)
         mkoptions(options, parser)
 
-        args = parser.parse_args(*args, **kwargs)
-        args = vars(args)
+        # parse into a dictionary
+        args = vars(parser.parse_args(*args, **kwargs))
+
+        # map args back onto the signature.
         pargs = []
         for param in positionals:
             if param.kind == param.VAR_POSITIONAL:
